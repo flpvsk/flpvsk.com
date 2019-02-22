b45ce4d32b6bcd04f5885b5d134e1c2d7b6f0885
Andrey Salomatin <filipovskii.off@gmail.com>
2019-02-22 12:00:42 +0100

    Separators.

--- site-flpvsk.com/pages/index.js
+++ site-flpvsk.com/pages/index.js
@@ -67,73 +67,62 @@ function HeroText() {
           '1fr'
         ]
       }>
-        <Box maxWidth={'43rem'} alignSelf='center'>
-          <BoxFlex
+        <Box alignSelf='center'>
+          <TextHeading
+            as='h1'
+            textStyle='h1'
             mt={[2, 2, 0, 0]}
             mb={[3, 3, 5]}
-            justifyContent='space-between'>
+            fontSize={[7, 7, 8, 8]}>
+              Andrey Salomatin
+          </TextHeading>
 
-            <TextHeading
-              as='h1'
-              textStyle='h1'
-              fontSize={[7, 7, 8, 8]}>
-                Andrey Salomatin
-            </TextHeading>
-
-            <Box pl={4} pt={2}>
-              <Logo
-                display={['none', 'none']}
-                pt={[2]}
-                pr={[0]}
-                size={[24, 24, 32, 40]}
-                innerColor={'white'}
-                outerColor={'black'} />
-            </Box>
-          </BoxFlex>
-          <TextBody pb={3}>
-            {`Programmer, product manager, founder, hobbyist musician.`}
-          </TextBody>
-          <TextBody pb={3}>
-            {`I helped build products like `}
-            <LinkText color='blacks.0' href='#Matterway'>
-              Matterway
-            </LinkText>
-            {`, `}
-            <LinkText color='blacks.0' href='#Polychops'>
-            Polychops
-            </LinkText>
-            {` and `}
-            <LinkText color='blacks.0' href='#Mindojo'>Mindojo</LinkText>
-            {`. `}
-            {`Helped organize `}
-            <LinkExternal color='blacks.0' href='https://moscowjs.ru'>
-              MoscowJS
-            </LinkExternal>
-            {`, `}
-            <LinkExternal
+          <Box maxWidth={'43rem'}>
+            <TextBody pb={3}>
+              {`Programmer, product manager, founder, hobbyist musician.`}
+            </TextBody>
+            <TextBody pb={3}>
+              {`I helped build products like `}
+              <LinkText color='blacks.0' href='#Matterway'>
+                Matterway
+              </LinkText>
+              {`, `}
+              <LinkText color='blacks.0' href='#Polychops'>
+              Polychops
+              </LinkText>
+              {` and `}
+              <LinkText color='blacks.0' href='#Mindojo'>Mindojo</LinkText>
+              {`. `}
+              {`Helped organize `}
+              <LinkExternal color='blacks.0' href='https://moscowjs.ru'>
+                MoscowJS
+              </LinkExternal>
+              {`, `}
+              <LinkExternal
+                  color='blacks.0'
+                  href='http://frontend-union-conf.github.io/func2016/'>
+                    Frontend Union Conf
+              </LinkExternal>
+              {` and `}
+              <LinkExternal
                 color='blacks.0'
-                href='http://frontend-union-conf.github.io/func2016/'>
-                  Frontend Union Conf
-            </LinkExternal>
-            {` and `}
-            <LinkExternal
-              color='blacks.0'
-              href='https://www.meetup.com/Kaizen-Berlin/'>
-                Kaizen Berlin
-            </LinkExternal>
-            {`.`}
-          </TextBody>
-          <TextBody pb={3}>
-            {`Currently I’m in the process of starting a `}
-            {`product research and development agency called `}
-            <LinkExternal color='blacks.0' href='https://mean.computer'>
-              Mean Computer
-            </LinkExternal>
-            {`.`}
-          </TextBody>
-          <TextBody pb={5}>
-            {`I write, speak and podcast about things I’m interested in.`}
-          </TextBody>
+                href='https://www.meetup.com/Kaizen-Berlin/'>
+                  Kaizen Berlin
+              </LinkExternal>
+              {`.`}
+            </TextBody>
+            <TextBody pb={3}>
+              {`Currently I’m in the process of starting a `}
+              {`product research and development agency called `}
+              <LinkExternal color='blacks.0' href='https://mean.computer'>
+                Mean Computer
+              </LinkExternal>
+              {`.`}
+            </TextBody>
+            <TextBody pb={5}>
+              {`I write, speak and podcast about things I’m interested in.`}
+            </TextBody>
+          </Box>
 
           <Box>
             <IconLinkTwitter height={24} />
@@ -301,6 +290,12 @@ function RecentPosts({ theme }) {
   );
 }
 
+function Work() {
+  return (
+    <BoxGrid />
+  );
+}
+
 const Home = withTheme(({ theme }) => {
   return (
     <BoxGrid
@@ -319,6 +314,13 @@ const Home = withTheme(({ theme }) => {
             <HeroText />
         </HeroSection>
         <RecentPosts theme={theme}/>
+        <BoxFlex mt={[4, 4, 4, 6]}>
+          <img
+            width='100%'
+            height='60px'
+            src='/static/separator-1.svg' alt='Separator' />
+        </BoxFlex>
+        <Work />
         <Footer />
     </BoxGrid>
   );
