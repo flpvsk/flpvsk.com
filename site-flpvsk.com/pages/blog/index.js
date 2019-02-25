1b40591337f27439c4b70b8716ef64005499c8f6
Andrey Salomatin <filipovskii.off@gmail.com>
2019-02-25 14:39:28 +0100

    Refactor Menu into a separate file.

--- site-flpvsk.com/pages/blog/index.js
+++ site-flpvsk.com/pages/blog/index.js
@@ -10,93 +10,10 @@ import BoxFlex from '~/shared/BoxFlex';
 import LinkNoDecoration from '~/shared/LinkNoDecoration';
 import Caption from '~/shared/Caption';
 
-import Logo from '~/shared/Logo';
+import Menu from '~/shared/Menu';
 
 import siteInfo from '~/siteInfo';
 
-const menuItems = [
-  {
-    id: 'main',
-    title: 'Main',
-    href: '/',
-  },
-
-  {
-    id: 'blog',
-    title: 'Blog',
-    href: '/blog',
-  },
-
-  {
-    id: 'talks',
-    title: 'Talks',
-    href: '/talks',
-  },
-
-  {
-    id: 'music',
-    title: 'Music',
-    href: '/music',
-  },
-];
-
-const MenuItem = styled(BoxFlex)(
-  props => {
-    const { white, black } = props.theme.colors;
-    const { isSelected } = props;
-    return {
-      backgroundColor: isSelected ? black : white,
-      color: isSelected ? white : black,
-
-      '&:hover': {
-        backgroundColor: props.theme.colors.black,
-        color: props.theme.colors.white,
-      }
-    };
-  }
-);
-
-function Menu({ items, currentItemId }) {
-  const itemsMapped = items.map(item => {
-    return (
-      <MenuItem
-        key={item.id}
-        mr={-1}
-        ml={4}
-        height={60}
-        alignItems={'stretch'}
-        isSelected={currentItemId === item.id}
-      >
-        <Link href={item.href} passHref prefetch>
-          <LinkNoDecoration
-            display='flex'
-            alignItems='center'
-            textStyle='caption'
-            fontSize={2}
-            color='inherit'
-            pr={1}
-            pl={1}
-          >
-            {item.title}
-          </LinkNoDecoration>
-        </Link>
-      </MenuItem>
-    );
-  });
-
-  return (
-    <BoxFlex
-      pl={[2, 3, 4]}
-      pr={[2, 3, 4]}
-      alignItems='center'
-      borderBottom='1px solid'
-      borderColor='black'
-    >
-      <Logo size={40} innerColor={'primary'} outerColor='black' />
-      {itemsMapped}
-    </BoxFlex>
-  );
-}
 
 export default function Blog() {
   return (
@@ -104,7 +21,10 @@ export default function Blog() {
       gridTemplateColumns={'100vw'}
       gridTemplateRows={'auto'}
     >
-      <Menu items={menuItems} currentItemId='blog' />
+      <Head>
+        <title>{siteInfo.blogName}</title>
+      </Head>
+      <Menu currentItemId='blog' />
     </BoxGrid>
   );
 }
---
+++ site-flpvsk.com/shared/Menu.js
@@ -0,0 +1,91 @@
+import Link from 'next/link'
+import styled from '@emotion/styled';
+
+import BoxFlex from '~/shared/BoxFlex';
+import LinkNoDecoration from '~/shared/LinkNoDecoration';
+
+import Logo from '~/shared/Logo';
+
+export const menuItems = [
+  {
+    id: 'main',
+    title: 'Main',
+    href: '/',
+  },
+
+  {
+    id: 'blog',
+    title: 'Blog',
+    href: '/blog',
+  },
+
+  {
+    id: 'talks',
+    title: 'Talks',
+    href: '/talks',
+  },
+
+  {
+    id: 'music',
+    title: 'Music',
+    href: '/music',
+  },
+];
+
+const MenuItem = styled(BoxFlex)(
+  props => {
+    const { white, black } = props.theme.colors;
+    const { isSelected } = props;
+    return {
+      backgroundColor: isSelected ? black : white,
+      color: isSelected ? white : black,
+
+      '&:hover': {
+        backgroundColor: props.theme.colors.black,
+        color: props.theme.colors.white,
+      }
+    };
+  }
+);
+
+export default function Menu({ items=menuItems, currentItemId }) {
+  const itemsMapped = items.map(item => {
+    return (
+      <MenuItem
+        key={item.id}
+        mr={-1}
+        ml={[ 3, 4 ]}
+        height={60}
+        alignItems={'stretch'}
+        isSelected={currentItemId === item.id}
+      >
+        <Link href={item.href} passHref prefetch>
+          <LinkNoDecoration
+            display='flex'
+            alignItems='center'
+            textStyle='caption'
+            fontSize={2}
+            color='inherit'
+            pr={1}
+            pl={1}
+          >
+            {item.title}
+          </LinkNoDecoration>
+        </Link>
+      </MenuItem>
+    );
+  });
+
+  return (
+    <BoxFlex
+      pl={[2, 3, 4]}
+      pr={[2, 3, 4]}
+      alignItems='center'
+      borderBottom='1px solid'
+      borderColor='black'
+    >
+      <Logo size={40} innerColor={'primary'} outerColor='black' />
+      {itemsMapped}
+    </BoxFlex>
+  );
+}
